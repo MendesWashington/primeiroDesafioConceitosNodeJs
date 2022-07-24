fe2c24e775f037ec83b339712c24141d25bfe548
Washington Mendes dos Santos <washington.mendes@ibm.com>
2022-07-24 18:39:40 -0300

    Adicionado documentação com o swagger

--- src/server.js
+++ src/server.js
@@ -1,3 +1,17 @@
-const app = require('./');
+const app = require("./");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../swagger.json");
+
+const options = {
+  swaggerOptions: {
+    validatorUrl: null,
+  },
+};
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, options)
+);
 
 app.listen(3333);
